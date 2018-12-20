806130a6320b00b375b2cbaf9921d2703a1d72f5
elizabith.costey <costey.ea@gmail.com>
2018-12-20 17:35:29 -0500

    Added book cover images and downvote function

--- main.js
+++ main.js
@@ -1,47 +1,61 @@
 const submissionComponent = {
     template: 
-    ` <div style="display: flex; width: 100%">
-    <figure class="media-left">
-    <i class="fas fa-book"></i>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <p>
-          <strong>
-            <a v-bind:href="submission.url" class="has-text-info">
-              {{ submission.title }}
-            </a>
-            <span class="tag is-small">#{{ submission.id }}</span>
-          </strong>
-          <br>
-            {{ submission.description }}
-          <br>
-          <small class="is-size-7">
-          Author: {{ submission.author }}
-          </small>
-        </p>
-      </div>
-    </div>
-    <div class="media-right">
-      <span class="icon is-small" v-on:click="upvote(submission.id)">
-        <i class="fa fa-chevron-up"></i>
-        <strong class="has-text-info">{{ submission.votes }}</strong>
-      </span>
-    </div>
-  </div>`
-    
-    ,
+        `<div style="display: flex; width: 100%">
+            <figure class="media-left">
+                <div class="coverSpace">
+                    <img class="cover"
+                        v-bind:src="submission.img">
+                </div>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>
+                            <a v-bind:href="submission.url" class="has-text-info">
+                                {{ submission.title }}
+                            </a>
+                            <span class="tag is-small">#{{ submission.id }}</span>
+                        </strong>
+                    <br>
+                        {{ submission.description }}
+                    <br>
+                        <small class="is-size-7">
+                            Author: {{ submission.author }}
+                        </small>
+                    </p>
+                </div>
+            </div>
+            <div class="media-right">
+                <span class="icon is-small" v-on:click="upvote(submission.id)">
+                    <i class="fa fa-chevron-up"></i>
+                </span>
+                <strong class="has-text-info">
+                    {{ submission.votes }}
+                </strong>
+                <span class="icon is-small" v-on:click="downvote(submission.id)">
+                    <i class="fa fa-chevron-down"></i>
+                </span>
+            </div>
+        </div>`,
     props: ['submission', 'submissions'],
-  methods: {
-    upvote(submissionId) {
-      const submission = this.submissions.find(
-        submission => submission.id === submissionId
-      );
-      submission.votes++;
+    methods: {
+        upvote(submissionId) {
+            let submission = this.submissions.find(
+                submission => submission.id === submissionId
+        );
+        submission.votes++;
+        },
+        downvote(submissionId) {
+            let submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes--;
+        }
     }
-  }
 };
 
+// Create new vue and seed the data from seed.js.
+// Sort the data by submission.votes
 new Vue({
     el: '#app',
     data: {
