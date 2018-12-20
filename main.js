1229a38c3d6be7f03ebae72ebd40e23305b22324
elizabith.costey <costey.ea@gmail.com>
2018-12-20 12:19:03 -0500

    Added 3 files and seeded starting data for first vue component.

---
+++ main.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+      submissions: Seed.submissions 
+    }
+  });
+
+//Tutorial From: Hassan Djirdeh. “Fullstack Vue.js.”
---
+++ seed.js
@@ -0,0 +1,30 @@
+window.Seed = (function () {
+    const submissions = [
+      {
+        id: 1,
+        title: 'Matilda',
+        description: 'Matilda is a book by British writer Roald Dahl. It was published in 1988 by Jonathan Cape in London, with 232 pages and illustrations by Quentin Blake.',
+        url: '#',
+        votes: 16,
+        author: 'Roald Dahl',
+    },
+    {
+        id: 2,
+        title: 'Where the Wild Things Are',
+        description: 'Where the Wild Things Are is a 1963 children\'s picture book by American writer and illustrator Maurice Sendak, originally published by Harper & Row.',
+        url: '#',
+        votes: 13,
+        author: 'Maurice Sendak',
+    },
+    {
+        id: 3,
+        title: 'Fancy Nancy: Stellar Stargazer!',
+        description: 'Fancy Nancy is a 2005 childrens picture book written by Jane O\'Connor and illustrated by Robin Preiss Glasser. The book spent nearly 100 weeks on the New York Times Best Seller list, launching a series that now numbers upwards of 80 books, selling 30 million volumes.',
+        url: '#',
+        votes: 20,
+        author: 'Jane O\'Connor',
+    }
+];
+
+return { submissions: submissions };
+}());
