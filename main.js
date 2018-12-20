1d9945f140c4353a192d8b2a8766702ce6ad65f4
elizabith.costey <costey.ea@gmail.com>
2018-12-20 14:59:13 -0500

    Added submissionComponent to main.js from index.html and passed props

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+const submissionComponent = {
+    template: 
+    ` <div style="display: flex; width: 100%">
+    <figure class="media-left">
+    <i class="fas fa-book"></i>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>
+            <a v-bind:href="submission.url" class="has-text-info">
+              {{ submission.title }}
+            </a>
+            <span class="tag is-small">#{{ submission.id }}</span>
+          </strong>
+          <br>
+            {{ submission.description }}
+          <br>
+          <small class="is-size-7">
+          Author: {{ submission.author }}
+          </small>
+        </p>
+      </div>
+    </div>
+    <div class="media-right">
+      <span class="icon is-small" v-on:click="upvote(submission.id)">
+        <i class="fa fa-chevron-up"></i>
+        <strong class="has-text-info">{{ submission.votes }}</strong>
+      </span>
+    </div>
+  </div>`
+    
+    ,
+    props: ['submission', 'submissions'],
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        submission => submission.id === submissionId
+      );
+      submission.votes++;
+    }
+  }
+};
+
 new Vue({
     el: '#app',
     data: {
@@ -10,14 +54,9 @@ new Vue({
             });
         }
     },
-    methods: {
-        upvote(submissionId) {
-            const submission = this.submissions.find(
-                submission => submission.id === submissionId
-            );
-            submission.votes++;
-        }
-    }
+    components: {
+        'submission-component': submissionComponent
+    },
 });
 
 //Tutorial From: Hassan Djirdeh. “Fullstack Vue.js.”
